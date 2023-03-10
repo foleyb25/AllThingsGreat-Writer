c7b129488f8ea2b4896d0e7ed08f3b0cc20c0d96
foleyb25 <foleyb25@gmail.com>
2023-03-09 20:18:28 -0700

    Adding tag input box, moods input box, and color scheme for moods in tailwind config

--- src/components/ArticleEditorComponent.vue
+++ src/components/ArticleEditorComponent.vue
@@ -1,162 +1,345 @@
 <template>
-    <div>
-        <div>
-            <label for="title" class="block m-2 text-sm font-medium text-gray-900 dark:text-gray-300">Title</label>
-            <input type="text" id="title" v-model="state.title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Title..." required>
-        </div>
-        <div>
-            <label for="title" class="block m-2 text-sm font-medium text-gray-900 dark:text-gray-300">Body</label>
-            <ckeditor v-model="state.editorData" :config="state.editorConfig"></ckeditor>
-        </div>
-        <div>
-            <label for="category" class="block m-2 text-sm font-medium text-gray-900 dark:text-gray-300">Category</label>
-            <select name="category" v-model="state.category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-                <option value="AllThingsGreat">AllThingsGreat</option>
-                <option value="Sports">Sports</option>
-                <option value="Entertainment">Entertainment</option>
-                <option value="Outdoors">Outdoors</option>
-                <option value="Gaming">Gaming</option>
-                <option value="Investments">Investments</option>
-                <option value="History">History</option>
-            </select>
-        </div>
-        <div>
-            <label for="img" class="block m-2 text-sm font-medium text-gray-900 dark:text-gray-300">Article Thumbnail Preview</label>
-            <div class="grid gap-4 sm:gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
-                <ArticleComponent :title="state.title" numcomments="XX" :category="state.category" :imageUrl="state.imagePath"></ArticleComponent>
-            </div>
-        </div>
-        <div class="mt-4">
-            <input :v-on="state.imagePath" @click.prevent="handleGetAwsImages" type="file" @change="onFileChange">
-        </div>
-        <div v-if="error">Error: {{error}}</div>
-        <div v-if="!loading" class="mt-4 flex flex-row">
-            <button v-if="!props.article" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded disabled:opacity-25" :disabled="state.isDisabled" @click="handleSubmit()">Create</button>
-            <button v-if="props.article" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded disabled:opacity-25" :disabled="state.isDisabled" @click="handleUpdate()">Update</button>
-            <button class=" ml-8 bg-yellow-200 hover:bg-yellow-400 text-black font-bold py-2 px-4 border border-yellow-400 rounded disabled:opacity-25" @click="handleSaveDraft">Save Draft</button>
-        </div>
-        
-        <label for="img" class="block m-2 mt-20 text-sm font-medium text-gray-900 dark:text-gray-300">Article Preview</label>
-        <div class="bg-white">
-            <span v-html="state.editorData"></span>
-        </div>
+	<div>
+		<div>
+			<label
+				for="title"
+				class="block m-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+				>Title</label
+			>
+			<input
+				type="text"
+				id="title"
+				v-model="state.title"
+				class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				placeholder="Title..."
+				required
+			/>
+		</div>
+		<div>
+			<label
+				for="title"
+				class="block m-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+				>Body</label
+			>
+			<ckeditor
+				v-model="state.editorData"
+				:config="state.editorConfig"
+			></ckeditor>
+		</div>
+		<div class="flex flex-col mt-4">
+			<label
+				for="category"
+				class="block m-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+				>Category</label
+			>
+			<select
+				name="category"
+				v-model="state.category"
+				class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				required
+			>
+				<option value="AllThingsGreat">AllThingsGreat</option>
+				<option value="Sports">Sports</option>
+				<option value="Entertainment">Entertainment</option>
+				<option value="Outdoors">Outdoors</option>
+				<option value="Gaming">Gaming</option>
+				<option value="Investments">Investments</option>
+				<option value="History">History</option>
+			</select>
+			<label
+				for="moods"
+				class="block m-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+				>Moods</label
+			>
+			<div v-for="mood in state.moodsOptions" class="m-2">
+				<input
+					v-model="state.moods"
+					type="checkbox"
+					:name="mood"
+					:value="mood"
+					class="mr-1"
+				/>
+				<label :for="mood"> {{ mood }}</label
+				><br />
+			</div>
+			<div v-for="mood in state.moods">
+				{{ mood }}
+			</div>
 
-         <!-- Modal component -->
-         <transition name="fade" appear>
-            <ImagePickerModalComponent v-if="state.showModal" v-model:imageUrls="state.awsImageUrls" @close-modal="state.showModal = false" @select-image="selectImage"/>
-         </transition>
-       
-    </div>
+			<div>
+				<label
+					for="tags"
+					class="block m-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+					>Tags</label
+				>
+				<input
+					type="text"
+					id="tags"
+					v-on:keydown.enter="pushTag"
+					v-on:keydown.,="pushTag"
+					v-on:keyup.enter="clearInputField"
+					v-on:keyup.,="clearInputField"
+					class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+					placeholder="Enter tags seperated by commas or enter..."
+					required
+				/>
+			</div>
+			<div class="m-4">
+				<div
+					v-for="tag in state.tags"
+					class="m-2 inline-block p-2 border border-black border-lg rounded-lg"
+				>
+					<span class="p-1">{{ tag }} </span>
+					<span
+						class="p-1 cursor-pointer font-bold border bg-gray-300 rounded-full"
+						@click="removeTag(tag)"
+						>X</span
+					>
+				</div>
+			</div>
+		</div>
+		<div>
+			<label
+				for="img"
+				class="block m-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+				>Article Thumbnail Preview</label
+			>
+			<div
+				class="grid gap-4 sm:gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3"
+			>
+				<ArticleComponent
+					:title="state.title"
+					numcomments="XX"
+					:category="state.category"
+					:imageUrl="state.imagePath"
+				></ArticleComponent>
+			</div>
+		</div>
+		<div class="mt-4">
+			<input
+				:v-on="state.imagePath"
+				@click.prevent="handleGetAwsImages"
+				type="file"
+				@change="onFileChange"
+			/>
+		</div>
+		<div v-if="error">Error: {{ error }}</div>
+		<div v-if="!loading" class="mt-4 flex flex-row">
+			<button
+				v-if="!props.article"
+				class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded disabled:opacity-25"
+				:disabled="state.isDisabled"
+				@click="handleSubmit()"
+			>
+				Create
+			</button>
+			<button
+				v-if="props.article"
+				class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded disabled:opacity-25"
+				:disabled="state.isDisabled"
+				@click="handleUpdate()"
+			>
+				Update
+			</button>
+			<button
+				class="ml-8 bg-yellow-200 hover:bg-yellow-400 text-black font-bold py-2 px-4 border border-yellow-400 rounded disabled:opacity-25"
+				@click="handleSaveDraft"
+			>
+				Save Draft
+			</button>
+		</div>
+
+		<label
+			for="img"
+			class="block m-2 mt-20 text-sm font-medium text-gray-900 dark:text-gray-300"
+			>Article Preview</label
+		>
+		<div class="bg-white">
+			<span v-html="state.editorData"></span>
+		</div>
+
+		<!-- Modal component -->
+		<transition name="fade" appear>
+			<ImagePickerModalComponent
+				v-if="state.showModal"
+				v-model:imageUrls="state.awsImageUrls"
+				@close-modal="state.showModal = false"
+				@select-image="selectImage"
+			/>
+		</transition>
+	</div>
 </template>
 
-<script setup> 
-import ArticleComponent from "../components/ArticleComponent.vue"
-import ImagePickerModalComponent from "./article/ImagePickerModalComponent.vue"
-import { useAuth0 } from '@auth0/auth0-vue';
-import { getImageUrls, createNewArticle} from "../services/apiRequest.service";
-import { reactive } from 'vue';
-import { storeToRefs } from 'pinia'
-import { useWriterStore } from '../stores/writer.store'
+<script setup>
+import ArticleComponent from "../components/ArticleComponent.vue";
+import ImagePickerModalComponent from "./article/ImagePickerModalComponent.vue";
+import { useAuth0 } from "@auth0/auth0-vue";
+import { getImageUrls, createNewArticle } from "../services/apiRequest.service";
+import { reactive } from "vue";
+import { storeToRefs } from "pinia";
+import { useWriterStore } from "../stores/writer.store";
 
-const {error, loading } = storeToRefs(useWriterStore())
-const { saveDraft } = useWriterStore()
+const { error, loading } = storeToRefs(useWriterStore());
+const { saveDraft } = useWriterStore();
 
-const props = defineProps(['article', 'draft'])
+const props = defineProps(["article", "draft"]);
 
 const state = reactive({
-    editorConfig: {
-        toolbar: [
-            {name: 'utils', items: ['Source', 'Scayt']},
-            {name: 'styling', items: ['Bold', 'Italic', 'Underline','Strike']},
-            {name: 'formatting', items: ['NumberedList', 'BulletedList', 'Blockquote']},
-            {name: 'linking', items: ['Link', 'Unlink', 'Image', "Iframe"]}
-        ],
-        height: "400px",
-        allowedContent: true,
-    },
-    editorData: props.article ? props.article.bodyHTML : props.draft  ? props.draft.bodyHTML : "<p>Write your masterpiece...</p>",
-    title: props.article ? props.article.title : props.draft ? props.draft.title : "",
-    moods: props.article ? props.article.moods : props.draft ? props.draft.moods : [],
-    tags: props.article ? props.article.tags : props.draft ? props.draft.tags : [],
-    category: props.article ? props.article.category : props.draft ? props.draft.category : "AllThingsGreat",
-    isDisabled: props.article ? false : true,
-    btnText: props.article ? "Update" : "Create",
-    imagePath: props.article ? props.article.imageUrl : "/src/assets/images/1669432796163-181228722+missing_img.jpeg",
-    awsImageUrls: [],
-    showModal: false,
-    user: useAuth0().user
-})
+	editorConfig: {
+		toolbar: [
+			{ name: "utils", items: ["Source", "Scayt"] },
+			{
+				name: "styling",
+				items: ["Bold", "Italic", "Underline", "Strike"],
+			},
+			{
+				name: "formatting",
+				items: ["NumberedList", "BulletedList", "Blockquote"],
+			},
+			{
+				name: "linking",
+				items: ["Link", "Unlink", "Image", "Iframe"],
+			},
+		],
+		height: "400px",
+		allowedContent: true,
+	},
+	editorData: props.article
+		? props.article.bodyHTML
+		: props.draft
+		? props.draft.bodyHTML
+		: "<p>Write your masterpiece...</p>",
+	title: props.article
+		? props.article.title
+		: props.draft
+		? props.draft.title
+		: "",
+	moods: props.article
+		? props.article.moods
+		: props.draft
+		? props.draft.moods
+		: [],
+	moodsOptions: [
+		"Sarcastic",
+		"Mind-blowing",
+		"Inspirational",
+		"Informative",
+		"Humorous",
+		"Analytical",
+		"Creative",
+		"Provocative",
+		"Introspective",
+		"Nostalgic",
+	],
+	tags: props.article
+		? props.article.tags
+		: props.draft
+		? props.draft.tags
+		: [],
+	category: props.article
+		? props.article.category
+		: props.draft
+		? props.draft.category
+		: "AllThingsGreat",
+	isDisabled: props.article ? false : true,
+	btnText: props.article ? "Update" : "Create",
+	imagePath: props.article
+		? props.article.imageUrl
+		: "/src/assets/images/1669432796163-181228722+missing_img.jpeg",
+	awsImageUrls: [],
+	showModal: false,
+	user: useAuth0().user,
+});
 
-console.log("EDITOR DATA: ", state.editorData)
+console.log("EDITOR DATA: ", state.editorData);
 
 const handleGetAwsImages = () => {
-    if(state.awsImageUrls.length > 0) {
-        state.showModal = true
-        return
-    }
-    getImageUrls()
-    .then((result) => {
-        if(result.status >= 200 && result.status < 300)
-            console.log(result)
-            state.awsImageUrls = result.data
-            state.showModal = true
-    }).catch( (err) => {
-        console.log(err)
-    })
-}
+	if (state.awsImageUrls.length > 0) {
+		state.showModal = true;
+		return;
+	}
+	getImageUrls()
+		.then((result) => {
+			if (result.status >= 200 && result.status < 300)
+				console.log(result);
+			state.awsImageUrls = result.data;
+			state.showModal = true;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
 
 const selectImage = (url) => {
-    state.imagePath = url
-    state.isDisabled = false
-}
+	state.imagePath = url;
+	state.isDisabled = false;
+};
+
+const pushTag = (e) => {
+	if (!e.target.value) return;
+	if (e.key === "Enter" || e.key === ",") {
+		var tag = e.target.value.replace(/,/g, "");
+		tag = tag.trim();
+		console.log("Pressed enter");
+		state.tags.push(e.target.value);
+		e.target.value = "";
+	}
+};
+
+const removeTag = (index) => {
+	console.log("INDEX: ", index);
+	state.tags = state.tags.filter((item) => item !== index);
+};
+
+const clearInputField = (e) => {
+	e.target.value = "";
+};
 
 const handleSubmit = async () => {
-    const mongoId = state.user.mongoId
-    const formdata = {
-        title: state.title,
-        bodyHTML: state.editorData,
-        imageUrl: state.imagePath,
-        writer: mongoId,
-        moods: state.moods,
-        tags: state.tags
-    }
-    
-    createNewArticle(formdata)
-        .then( (data) => {
-        console.log(data.data)
-    }).catch( (err) => {
-        console.log(err)
-    })
-}
+	const mongoId = state.user.mongoId;
+	const formdata = {
+		title: state.title,
+		bodyHTML: state.editorData,
+		imageUrl: state.imagePath,
+		writer: mongoId,
+		moods: state.moods,
+		tags: state.tags,
+	};
+
+	createNewArticle(formdata)
+		.then((data) => {
+			console.log(data.data);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
 
 const handleSaveDraft = async (e) => {
-    //Patch request
-    const body = {
-        title: state.title,
-        bodyHTML: state.editorData,
-        imageUrl: state.imagePath,
-        category: state.category,
-        tags: state.tags,
-        moods: state.moods
-    }
-    console.log("BODYHTML: ", body.bodyHTML)
-    await saveDraft(body)
-}
+	//Patch request
+	const body = {
+		title: state.title,
+		bodyHTML: state.editorData,
+		imageUrl: state.imagePath,
+		category: state.category,
+		tags: state.tags,
+		moods: state.moods,
+	};
+	console.log("BODYHTML: ", body.bodyHTML);
+	await saveDraft(body);
+};
 
-const handleUpdate = async (e) => {
-
-}
-
+const handleUpdate = async (e) => {};
 </script>
 
 <style>
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity .4s linear;
+	transition: opacity 0.4s linear;
 }
 
 .fade-enter,
 .fade-leave-to {
-  opacity: 0;
+	opacity: 0;
 }
 </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,40 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sarcastic: {
+          gray: "#7F8C8D",
+        },
+        mindBlowing: {
+          gold: "#FFD700",
+        },
+        inspirational: {
+          turquoise: "#00CED1",
+        },
+        informative: {
+          teal: "#008080"
+        },
+        humorous: {
+          diamond: "#F8F8FF"
+        },
+        analytical: {
+          gray: "#808080"
+        },
+        creative: {
+          orange: "#FFA500"
+        },
+        provocative: {
+          red: "#800000"
+        },
+        introspective: {
+          purple: "#483D8B"
+        },
+        nostalgic: {
+          beige: "#F5DEB3"
+        }
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin'),
