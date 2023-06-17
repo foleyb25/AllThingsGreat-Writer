654487b61a7ed8310642f0c942026f1bd4a531fc
foleyb25 <foleyb25@gmail.com>
2023-06-17 16:01:01 -0600

    Updating base path to / since custom domain is now used

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig( ({command, mode}) => {
   const env = loadEnv(mode, process.cwd(), "")
 
   return {
-    base: '/AllThingsGreatWriter-Hosting/',
+    base: '/',
     plugins: [vue()],
   }
   
