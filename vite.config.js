632b85e4acde48559a39efc0bd4259b6f7b3b7f0
foleyb25 <foleyb25@gmail.com>
2023-06-17 15:35:44 -0600

    Update repo name + base path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig( ({command, mode}) => {
   const env = loadEnv(mode, process.cwd(), "")
 
   return {
-    base: '/AllThingsGreat-Public/',
+    base: '/AllThingsGreatWriter-Hosting/',
     plugins: [vue()],
   }
   
