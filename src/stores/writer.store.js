20aca356d1d674216966ad3980c0b6a76c1daab5
foleyb25 <foleyb25@gmail.com>
2023-06-18 01:41:49 -0600

    Updating tracers for more detailed data

--- src/stores/writer.store.js
+++ src/stores/writer.store.js
@@ -29,8 +29,8 @@ export const useWriterStore = defineStore('writerStore', {
         const {setNotification} = useGlobalNotificationStore()
         const response = await getAuthenticatedWriter()
         if (response.status === 'success') {
-          console.log("Writer: ", response.data)
-          console.log("PROD API URL: ", import.meta.env.VITE_AUTH0_AUDIENCE)
+          console.log("Writer: ", response)
+          console.log("PROD API URL: ", import.meta.env.VITE_API_SERVER_URL)
           this.writer = response.data
           // setNotification(response.message, 'success', 'bg-green-300')
         } else {
