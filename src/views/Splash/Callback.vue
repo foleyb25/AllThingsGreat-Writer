b619b226ffba367c571711ffc006f16b27c9248b
foleyb25 <foleyb25@gmail.com>
2023-06-17 22:38:00 -0600

    Adding logic inside of callback view to redirect to /

--- src/views/Splash/Callback.vue
+++ src/views/Splash/Callback.vue
@@ -1,3 +1,21 @@
 <template>
-    <div class="h-screen w-full bg-gray-100"></div>
+	<div>Loading...</div>
 </template>
+
+<script>
+import { onMounted } from "vue";
+import { useAuth0 } from "@auth0/auth0-vue";
+
+export default {
+	name: "Callback",
+	setup() {
+		const { handleRedirectCallback } = useAuth0();
+
+		onMounted(async () => {
+			await handleRedirectCallback();
+			// Navigate the user to another route after authentication is complete
+			this.$router.push("/");
+		});
+	},
+};
+</script>
